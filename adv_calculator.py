efff4c19cfe3668b12b068a1686ed6632a216446
Santiago Basulto <santiago.basulto@gmail.com>
2019-09-17 15:06:38 -0300

    Update adv_calculator.py

--- adv_calculator.py
+++ adv_calculator.py
@@ -1,62 +1,20 @@
 def add(*args):
-    return sum(args)
-
-    # ALTERNATIVE SOLUTION
-    # total = 0
-    # for arg in args:
-    #     total += arg 
-    # return total 
-
+    pass
 
 def multiply(*args):
-    total = 1 
-    for arg in args:
-        total *= arg 
-    return total 
-
+    pass
 
 def maximum(*args):
-    return max(args)
-    
-    # ALTERNATIVE SOLUTION
-    # greatest_num = None
+    pass
 
-    # for arg in args: 
-    #     if greatest_num == None or arg > greatest_num:
-    #         greatest_num = arg
-    # return greatest_num
-  
 def minimum(*args):
-    return min(args)
-
-    # ALTERNATIVE SOLUTION
-    # lowest_num = None 
-
-    # for arg in args: 
-    #     if lowest_num == None or arg < lowest_num:
-    #         lowest_num = arg
-    # return lowest_num
+    pass
 
 def mean(*args): 
-    return sum(args) / len(args)
-
-    # ALTERNATIVE SOLUTION
-    # total = 0 
-    # for arg in args:
-    #     total += arg 
-
-    # return total/len(args)
+    pass
 
 def variance(*args):
-    _mean = mean(*args)
-    variance_sum_total = 0
-    for arg in args:
-        variance_sum_total += (arg - _mean) ** 2
-    return variance_sum_total / len(args)
-
-
+    pass
 
 def std(*args):
-    def sqrt(x):
-        return x**(1/2)
-    return sqrt(variance(*args))
+    pass
